4421b01c2177fb50416cb40164aead74b0ac25b2
Jason Lenoble <jason.lenoble@gmail.com>
2016-12-22 18:48:01 +0100

    stderr output
    Used to output only on stdout and stderr used to be discarded (that
    is only buffered to be accessed later)

--- src/child-process-data.js
+++ src/child-process-data.js
@@ -106,6 +106,21 @@ function childProcessData(childProcess, options) {
       const str = data.toString(options.format);
       errMessages.push(str);
       allMessages.push(str);
+
+      var found = false;
+
+      dataCallbacks.some(obj => {
+        var res = str.match(obj.regexp);
+        if (res) {
+          found = true;
+          obj.callback(res[1], res[2], res[3], res[4]);
+        }
+        return found;
+      });
+
+      if (!found) {
+        process.stderr.write(chalk.yellow(str));
+      }
     });
 
     const callback = function(code) {
