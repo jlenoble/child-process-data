8f7179a9a39737f29197619e3f5d1958bf6decc5
Jason Lenoble <jason.lenoble@gmail.com>
2017-03-31 18:16:11 +0200

    Added method forgetUpTo()

--- src/extend-options.js
+++ src/extend-options.js
@@ -5,24 +5,67 @@ export default function extendOptions (options, childProcess, resolve, reject) {
 
   options.result = { // eslint-disable-line no-param-reassign
     childProcess, outMessages, errMessages, allMessages,
+
     out () {
       return this.outMessages.join('');
     },
+
     err () {
       return this.errMessages.join('');
     },
+
     all () {
       return this.allMessages.join('');
     },
+
     forget () {
       this.outMessages.length = 0;
       this.errMessages.length = 0;
       this.allMessages.length = 0;
     },
+
+    forgetUpTo (message, options = {}) {
+      const pattern = message instanceof RegExp ? message : new RegExp(message);
+      const {included} = options;
+      let aIdx = -1;
+
+      this.allMessages.some((msg, idx) => {
+        if (msg.match(pattern)) {
+          aIdx = idx;
+
+          if (included) {
+            if (msg === this.outMessages[0]) {
+              this.outMessages.shift();
+            } else {
+              this.errMessages.shift();
+            }
+          }
+
+          return true;
+        }
+
+        if (msg === this.outMessages[0]) {
+          this.outMessages.shift();
+        } else {
+          this.errMessages.shift();
+        }
+
+        return false;
+      });
+
+      if (aIdx === -1) {
+        this.allMessages.length = 0;
+        throw new Error(`"${message}" not found in messages up till now`);
+      }
+
+      this.allMessages.splice(0, included ? aIdx + 1 : aIdx);
+    },
   };
+
   options.resolve = () => { // eslint-disable-line no-param-reassign
     resolve(options.result);
   };
+
   options.reject = reject; // eslint-disable-line no-param-reassign
 
   return options;
--- test/forget.test.js
+++ test/forget.test.js
@@ -7,6 +7,10 @@ describe('Testing childProcessData', function () {
     return childProcessData(spawn('echo', args));
   };
 
+  const node = (...args) => {
+    return childProcessData(spawn('node', args));
+  };
+
   it(`childProcessData can forget buffered messages`, function () {
     return Promise.all([
       echo('Little silly message').then(res => {
@@ -18,6 +22,28 @@ describe('Testing childProcessData', function () {
         expect(res.all()).to.equal('Another little silly message\n');
         res.forget();
         expect(res.all()).to.equal('');
+      }),
+    ]);
+  });
+
+  it(`childProcessData can forget messages up to a point`, function () {
+    return Promise.all([
+      node('./test/examples/normal-exit.js').then(res => {
+        expect(res.out()).to.equal('lorem\nipsum\nsit\n');
+        expect(res.err()).to.equal('dolor\namet\n');
+        res.forgetUpTo('dolor\n', {included: true});
+        expect(res.out()).to.equal('sit\n');
+        expect(res.err()).to.equal('amet\n');
+        expect(res.allMessages).to.have.length(2);
+      }),
+      node('./test/examples/error-exit.js').catch(err => {
+        const res = err.result;
+        expect(res.out()).to.equal('lorem\nipsum\nsit\n');
+        expect(res.err()).to.equal('dolor\n' + res.errMessages[1]);
+        res.forgetUpTo('dolor\n');
+        expect(res.out()).to.equal('sit\n');
+        expect(res.err()).to.equal('dolor\n' + res.errMessages[1]);
+        expect(res.allMessages).to.have.length(3);
       }),
     ]);
   });
