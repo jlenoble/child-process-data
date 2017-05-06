cec2d73a7675071c05645d15b10178ab7e632d51
Jason Lenoble <jason.lenoble@gmail.com>
2017-05-06 20:23:44 +0200

    Removed watching
    Allows process to return naturally

--- test/gulpfiles/gulpfile_single-test.js
+++ test/gulpfiles/gulpfile_single-test.js
@@ -1,16 +1,15 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 
-const hello = function (done) {
+const hello = function () {
   for (let i = 0; i < 5; i++) {
     gutil.log(`Test message ${i}: Hello!`);
   }
-  done();
 };
 
 gulp.task('subtest', function (done) {
-  gulp.watch('gulpfile_single-test.js', hello);
-  hello(done);
+  hello();
+  done();
 });
 
 gulp.task('default', gulp.parallel('subtest'));
