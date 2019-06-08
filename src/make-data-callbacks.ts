4a29630e104919fe5484246d7421ba84a76b5797
Jason Lenoble <jason.lenoble@gmail.com>
2019-06-08 18:07:31 +0200

    Added types

--- src/make-data-callbacks.ts
+++ src/make-data-callbacks.ts
@@ -1,7 +1,14 @@
-export default function makeDataCallbacks (callbacks) {
-  return Object.keys(callbacks).map(key => ({
-    pattern: key,
-    regexp: new RegExp(key),
-    callback: callbacks[key],
-  }));
+import { DataCallbacks } from "./data-callbacks/handler";
+import { CallbackOptions } from "./options";
+
+export default function makeDataCallbacks(
+  callbacks: DataCallbacks
+): CallbackOptions[] {
+  return Object.keys(callbacks).map(
+    (key): CallbackOptions => ({
+      pattern: key,
+      regexp: new RegExp(key),
+      callback: callbacks[key]
+    })
+  );
 }
