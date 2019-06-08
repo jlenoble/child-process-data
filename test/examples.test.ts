651a551309a3e1ea567fd4cea47fd062ff4964a3
Jason Lenoble <jason.lenoble@gmail.com>
2019-06-08 20:13:35 +0200

    Converted to async funcs

--- test/examples.test.ts
+++ test/examples.test.ts
@@ -1,59 +1,63 @@
 import childProcessData from "../src/index";
 import { spawn } from "child_process";
 import { expect } from "chai";
+import { Result } from "../src/options";
 
-describe("Testing README.md examples", () => {
-  it(`Testing 'Simple usage' example`, () => {
+describe("Testing README.md examples", (): void => {
+  it(`Testing 'Simple usage' example`, async (): Promise<void> => {
     const proc1 = spawn("echo", ["Lorem ipsum"]); // Spawns a child process
     const proc2 = spawn("echo", ["dolor sit amet"]); // Spawns another child
     // process
 
-    return Promise.all([
-      childProcessData(proc1).then(res => {
-        // Recovers output from proc1
-        expect(res.all()).to.equal("Lorem ipsum\n");
-      }),
-      childProcessData(proc2).then(res => {
-        // Recovers output from proc2
-        expect(res.all()).to.equal("dolor sit amet\n");
-      })
+    const [res1, res2]: [Result, Result] = await Promise.all([
+      childProcessData(proc1),
+      childProcessData(proc2)
     ]);
+
+    expect(res1.all()).to.equal("Lorem ipsum\n");
+    expect(res2.all()).to.equal("dolor sit amet\n");
   });
 
-  it(`Testing 'Accessing individual messages' example`, () => {
-    return childProcessData(
+  it(`Testing 'Accessing individual messages' example`, async (): Promise<
+    void
+  > => {
+    const res: Result = await childProcessData(
       spawn("node", ["./test/examples/normal-exit.js"])
-    ).then(res => {
-      expect(res.outMessages[0]).to.equal("lorem\n");
-      expect(res.outMessages[1]).to.equal("ipsum\n");
-      expect(res.outMessages[2]).to.equal("sit\n");
-      expect(res.errMessages[0]).to.equal("dolor\n");
-      expect(res.errMessages[1]).to.equal("amet\n");
-      expect(res.allMessages).to.include("lorem\n");
-      expect(res.allMessages).to.include("ipsum\n");
-      expect(res.allMessages).to.include("dolor\n");
-      expect(res.allMessages).to.include("sit\n");
-      expect(res.allMessages).to.include("amet\n");
-    });
+    );
+
+    expect(res.outMessages[0]).to.equal("lorem\n");
+    expect(res.outMessages[1]).to.equal("ipsum\n");
+    expect(res.outMessages[2]).to.equal("sit\n");
+    expect(res.errMessages[0]).to.equal("dolor\n");
+    expect(res.errMessages[1]).to.equal("amet\n");
+    expect(res.allMessages).to.include("lorem\n");
+    expect(res.allMessages).to.include("ipsum\n");
+    expect(res.allMessages).to.include("dolor\n");
+    expect(res.allMessages).to.include("sit\n");
+    expect(res.allMessages).to.include("amet\n");
   });
 
-  it(`Testing 'Accessing all messages' example`, () => {
-    return childProcessData(
+  it(`Testing 'Accessing all messages' example`, async (): Promise<void> => {
+    const res: Result = await childProcessData(
       spawn("node", ["./test/examples/normal-exit.js"])
-    ).then(res => {
-      expect(res.out()).to.equal("lorem\nipsum\nsit\n");
-      expect(res.err()).to.equal("dolor\namet\n");
-      ["lorem\n", "ipsum\n", "dolor\n", "sit\n", "amet\n"].forEach(word => {
+    );
+
+    expect(res.out()).to.equal("lorem\nipsum\nsit\n");
+    expect(res.err()).to.equal("dolor\namet\n");
+    ["lorem\n", "ipsum\n", "dolor\n", "sit\n", "amet\n"].forEach(
+      (word): void => {
         expect(res.all()).to.match(new RegExp(word));
-      });
-    });
+      }
+    );
   });
 
-  it(`Testing 'Accessing uncaught error messages' example`, () => {
-    return childProcessData(
-      spawn("node", ["./test/examples/error-exit.js"])
-    ).catch(_err => {
-      const res = _err.result;
+  it(`Testing 'Accessing uncaught error messages' example`, async (): Promise<
+    void
+  > => {
+    try {
+      await childProcessData(spawn("node", ["./test/examples/error-exit.js"]));
+    } catch (_err) {
+      const res: Result = _err.result;
 
       expect(res.outMessages[0]).to.equal("lorem\n");
       expect(res.outMessages[1]).to.equal("ipsum\n");
@@ -71,6 +75,6 @@ describe("Testing README.md examples", () => {
       expect(res.all()).to.equal(
         "lorem\nipsum\ndolor\nsit\n" + res.allMessages[4]
       );
-    });
+    }
   });
 });
