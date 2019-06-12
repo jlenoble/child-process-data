5edefcb1535cde447152482b33d1e749b91bec3c
Jason Lenoble <jason.lenoble@gmail.com>
2019-06-12 15:17:00 +0200

    Added various typings

--- test/forget.test.ts
+++ test/forget.test.ts
@@ -1,57 +1,71 @@
-import childProcessData from "../src/index";
+import childProcessData, { Result } from "../src/index";
 import { spawn } from "child_process";
 import { expect } from "chai";
 
-describe("Testing childProcessData", () => {
-  const echo = (...args) => {
+describe("Testing childProcessData", (): void => {
+  const echo = (...args: string[]): Promise<Result> => {
     return childProcessData(spawn("echo", args));
   };
 
-  const node = (...args) => {
+  const node = (...args: string[]): Promise<Result> => {
     return childProcessData(spawn("node", args));
   };
 
-  it(`childProcessData can forget buffered messages`, () => {
+  it(`childProcessData can forget buffered messages`, (): Promise<
+    [void, void]
+  > => {
     return Promise.all([
-      echo("Little silly message").then(res => {
-        expect(res.all()).to.equal("Little silly message\n");
-        res.forget();
-        expect(res.all()).to.equal("");
-      }),
-      echo("Another little silly message").then(res => {
-        expect(res.all()).to.equal("Another little silly message\n");
-        res.forget();
-        expect(res.all()).to.equal("");
-      })
+      echo("Little silly message").then(
+        (res): void => {
+          expect(res.all()).to.equal("Little silly message\n");
+          res.forget();
+          expect(res.all()).to.equal("");
+        }
+      ),
+      echo("Another little silly message").then(
+        (res): void => {
+          expect(res.all()).to.equal("Another little silly message\n");
+          res.forget();
+          expect(res.all()).to.equal("");
+        }
+      )
     ]);
   });
 
-  it(`childProcessData can forget messages up to a point`, () => {
+  it(`childProcessData can forget messages up to a point`, (): Promise<
+    [void, void | Result]
+  > => {
     return Promise.all([
-      node("./test/examples/normal-exit.js").then(res => {
-        expect(res.out()).to.equal("lorem\nipsum\nsit\n");
-        expect(res.err()).to.equal("dolor\namet\n");
-        res.forgetUpTo("dolor\n", { included: true });
-        expect(res.out()).to.equal("sit\n");
-        expect(res.err()).to.equal("amet\n");
-        expect(res.all()).to.equal("sit\namet\n");
-      }),
-      node("./test/examples/error-exit.js").catch(err => {
-        const res = err.result;
-        expect(res.out()).to.equal("lorem\nipsum\nsit\n");
-        expect(res.err()).to.equal("dolor\n" + res._errMessages[1]);
-        res.forgetUpTo("dolor\n");
-        expect(res.out()).to.equal("sit\n");
-        expect(res.err()).to.equal("dolor\n" + res._errMessages[1]);
-        expect(res.all()).to.equal("dolor\nsit\n" + res._errMessages[1]);
-      })
+      node("./test/examples/normal-exit.js").then(
+        (res): void => {
+          expect(res.out()).to.equal("lorem\nipsum\nsit\n");
+          expect(res.err()).to.equal("dolor\namet\n");
+          res.forgetUpTo("dolor\n", { included: true });
+          expect(res.out()).to.equal("sit\n");
+          expect(res.err()).to.equal("amet\n");
+          expect(res.all()).to.equal("sit\namet\n");
+        }
+      ),
+      node("./test/examples/error-exit.js").catch(
+        (err): void => {
+          const res = err.result;
+          expect(res.out()).to.equal("lorem\nipsum\nsit\n");
+          expect(res.err()).to.equal("dolor\n" + res._errMessages[1]);
+          res.forgetUpTo("dolor\n");
+          expect(res.out()).to.equal("sit\n");
+          expect(res.err()).to.equal("dolor\n" + res._errMessages[1]);
+          expect(res.all()).to.equal("dolor\nsit\n" + res._errMessages[1]);
+        }
+      )
     ]);
   });
 
-  it(`childProcessData can forget messages up to a point - special chars`, () => {
-    return Promise.all([
-      node("./test/examples/queue-bugfix.js").then(res => {
-        expect(res.out()).to.equal(`Starting 'default'...
+  it(`childProcessData can forget messages up to a point - special chars`, async (): Promise<
+    void
+  > => {
+    const res = await node("./test/examples/queue-bugfix.js");
+
+    expect(res.out()).to.equal(`Starting 'default'...
 Starting 'exec:transpile'...
 Starting 'exec:copy'...
 Task 'copy' (SRC): src/gulptask.js
@@ -70,8 +84,10 @@ Finished 'transpile' after 150 ms
 Finished 'exec:transpile' after 150 ms
 Finished 'default' after 150 ms
 `);
-        res.forgetUpTo(`Starting 'exec:transpile'...`, { included: true });
-        expect(res.out()).to.equal(`
+
+    res.forgetUpTo(`Starting 'exec:transpile'...`, { included: true });
+
+    expect(res.out()).to.equal(`
 Starting 'exec:copy'...
 Task 'copy' (SRC): src/gulptask.js
 Task 'copy' (SRC): 1 item
@@ -89,8 +105,10 @@ Finished 'transpile' after 150 ms
 Finished 'exec:transpile' after 150 ms
 Finished 'default' after 150 ms
 `);
-        res.forgetUpTo(`Starting 'exec:copy'...`, { included: true });
-        expect(res.out()).to.equal(`
+
+    res.forgetUpTo(`Starting 'exec:copy'...`, { included: true });
+
+    expect(res.out()).to.equal(`
 Task 'copy' (SRC): src/gulptask.js
 Task 'copy' (SRC): 1 item
 Task 'copy' (NWR): src/gulptask.js
@@ -107,10 +125,12 @@ Finished 'transpile' after 150 ms
 Finished 'exec:transpile' after 150 ms
 Finished 'default' after 150 ms
 `);
-        res.forgetUpTo(`Task 'copy' (SRC): src/gulptask.js`, {
-          included: true
-        });
-        expect(res.out()).to.equal(`
+
+    res.forgetUpTo(`Task 'copy' (SRC): src/gulptask.js`, {
+      included: true
+    });
+
+    expect(res.out()).to.equal(`
 Task 'copy' (SRC): 1 item
 Task 'copy' (NWR): src/gulptask.js
 Task 'copy' (NWR): 1 item
@@ -126,14 +146,14 @@ Finished 'transpile' after 150 ms
 Finished 'exec:transpile' after 150 ms
 Finished 'default' after 150 ms
 `);
-        res.forgetUpTo(`Task 'transpile' (NWR): 1 item`, { included: true });
-        expect(res.out()).to.equal(`
+
+    res.forgetUpTo(`Task 'transpile' (NWR): 1 item`, { included: true });
+
+    expect(res.out()).to.equal(`
 Task 'transpile' (DST): 1 item
 Finished 'transpile' after 150 ms
 Finished 'exec:transpile' after 150 ms
 Finished 'default' after 150 ms
 `);
-      })
-    ]);
   });
 });
