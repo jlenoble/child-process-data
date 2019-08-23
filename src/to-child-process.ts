5cdae4142ad4f9f1b93c750a2c212b5192632ec8
Jason Lenoble <jason.lenoble@gmail.com>
2019-08-23 20:27:23 +0200

    Convert SpawnArguments to ChildProcessWithReadableStdStreams

---
+++ src/to-child-process.ts
@@ -0,0 +1,22 @@
+import { spawn, SpawnOptionsWithoutStdio } from "child_process";
+import { ChildProcessWithReadableStdStreams } from "./child-process";
+
+export type SpawnArguments =
+  | [string]
+  | [string, SpawnOptionsWithoutStdio]
+  | [string, string[], SpawnOptionsWithoutStdio | undefined];
+
+export default function toChildProcess(
+  args: SpawnArguments
+): ChildProcessWithReadableStdStreams {
+  if (args.length === 3) {
+    const [command, _args, options] = args;
+    return spawn(command, _args, options);
+  } else if (args.length === 2) {
+    const [command, _args] = args;
+    return spawn(command, _args);
+  } else {
+    const [command] = args;
+    return spawn(command);
+  }
+}
